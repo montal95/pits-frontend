d004638d1fa565d237392844c27985deb87c608c
Sam M <sammontalvojr@gmail.com>
2020-09-18 13:42:55 -0500

    base react setup

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import { Provider } from "react-redux";
+import rootReducer from "./reducers/index";
+
+import { createStore } from "redux";
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
